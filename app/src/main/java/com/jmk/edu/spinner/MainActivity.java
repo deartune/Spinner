1b63dc6a8b86b4a75c406ce06c843fc0c58f2802
deartune <deartune@gmail.com>
2018-11-05 16:17:26 +0900

    intent적용

--- app/src/main/java/com/jmk/edu/spinner/MainActivity.java
+++ app/src/main/java/com/jmk/edu/spinner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jmk.edu.spinner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +22,7 @@ protected void onCreate(Bundle savedInstanceState) {
     setContentView(R.layout.activity_main);
     Spinner spinner=(Spinner)findViewById(R.id.spinner);
     List<String> list = new ArrayList<String>();
+    list.add("스피너를 선택해주세요");
     list.add("Apple");
     list.add("Mango");
     list.add("Banana");
@@ -34,10 +36,24 @@ protected void onCreate(Bundle savedInstanceState) {
 @Override
 public void onItemSelected(AdapterView<?> adapterView, View view,
         int position, long id) {
+    if(position==1){
+        Intent intent=null;
+        intent = new Intent(this,AppleActivity.class);
+        startActivity(intent);
+    }
+    if(position==2){
+        Intent intent=null;
+        intent = new Intent(this,MangoActivity.class);
+        startActivity(intent);
+    }
+    if(position==3){
+        Intent intent=null;
+        intent = new Intent(this,BananaActivity.class);
+        startActivity(intent);
+    }
+       // String seleted = ((TextView)view).getText().toString();
 
-        String seleted = ((TextView)view).getText().toString();
-
-    Toast.makeText(getApplicationContext(), seleted+"을(를) 선택했습니다.", 1).show();
+    //Toast.makeText(getApplicationContext(), seleted+"을(를) 선택했습니다.",1).show();
 
 
         }
